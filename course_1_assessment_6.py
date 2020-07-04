c80e071dca8d7b2a91ecebaec657653aaef08f49
sumanthprapulla <48341481+sumanthprapulla@users.noreply.github.com>
2020-07-04 19:41:23 +0530

    Update course_1_assessment_6.py

--- course_1_assessment_6.py
+++ course_1_assessment_6.py
@@ -16,30 +16,26 @@ for i in str_list:
     print(len(i))
 
 #Write code to count the number of characters in original_str using the accumulation pattern and assign the answer to a variable num_chars. Do NOT use the len function to solve the problem (if you use it while you are working on this problem, comment it out afterward!)
-mylist = []
-
-for i in range(41):
-    
-    mylist.append(i)
-    print(mylist)
-print(len(mylist))
+original_str = "The quick brown rhino jumped over the extremely lazy fox."
+num_chars=0
+for i in original_str:
+    num_chars=num_chars+1
+print(num_chars)
 
 #addition_str is a string with a list of numbers separated by the + sign. Write code that uses the accumulation pattern to take the sum of all of the numbers and assigns it to sum_val (an integer). (You should use the .split("+") function to split by "+" and int() to cast to an integer).
 addition_str = "2+5+10+20"
-sum_val = addition_str.split("+")
-
-print(sum(sum_val))
+nums = addition_str.split("+")
+val = [int(s) for s in nums]
+sum_val=sum(val)
 
 #week_temps_f is a string with a list of fahrenheit temperatures separated by the , sign. Write code that uses the accumulation pattern to compute the average (sum divided by number of items) and assigns it to avg_temp. Do not hard code your answer (i.e., make your code compute both the sum or the number of items in week_temps_f) (You should use the .split(",") function to split by "," and float() to cast to a float).
 week_temps_f = "75.1,77.7,83.2,82.5,81.0,79.5,85.7"
 temp = week_temps_f.split(",")
-print(temp)
-total = 0
-for ele in range(0, len(temp)):
-    total = total + temp[ele]
-    print(total)
+val = [float(s) for s in temp]
+total=sum(val)
 avg_temp = float(total)/len(temp)
 print(temp)
+
 
 #Write code to create a list of numbers from 0 to 67 and assign that list to the variable nums. Do not hard code the list.
 nums = range(68)
